f197ddf1ba691d71930214b2bd834159580287fc
KelvinBarros <kelvinbarros92@gmail.com>
2024-02-07 17:44:06 -0300

    terminar exercicios

---
+++ estudoslogica/exe12/contabanco.js
@@ -0,0 +1,12 @@
+function calcular(){
+    let conta = document.getElementById('nconta').value
+    let saldo = document.getElementById('saldo').value
+    let gasto = document.getElementById('gastos').value
+    let entra = document.getElementById('entrada').value
+
+    if(isNaN(nconta)){
+        alert('Campo não preenchido corretamente')
+    } else{
+        
+    }
+}
