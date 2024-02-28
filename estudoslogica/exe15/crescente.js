fd52e3e9d16fdfa3c3b1367e6b6de9727aa9f021
KelvinBarros <kelvinbarros92@gmail.com>
2024-02-28 16:34:18 -0300

    Update crescente.js

--- estudoslogica/exe15/crescente.js
+++ estudoslogica/exe15/crescente.js
@@ -7,11 +7,5 @@ function calcular(){
     let n2= Number(num2.value)
     let n3= Number(num3.value)
 
-    if(n1 > n2 && n1 > n3){
-        res.innerHTML=`${n1}, ${n2},${n3}`
-    } if(n2 > n1 && n2 > n3){
-        res.innerHTML=`${n2}, ${n1},${n3}`
-    } if(n3 > n1 && n3 > n2){
-        res.innerHTML=`${n3}, ${n2},${n3}`
-    }
+    /* EXEXCICIO IMCOMPLETO*/
 }
